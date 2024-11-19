1795697b58dc2f54305dcf770624832c68ce3ff7
Elisa Delannoy <elisadelannoy10@gmail.com>
2024-11-19 18:40:39 +0100

    printf-i-d

--- ft_printf.c
+++ ft_printf.c
@@ -33,16 +33,53 @@ int	ft_putstr(char *s)
 	}
 	return (0);
 }
+int	ft_convert10to16(int nb, char c)
+{
+	if (nb >= 16)
+	{
+		ft_convert10to16(nb / 16, c);
+		ft_convert10to16(nb % 16, c);
+	}
+	else if ((nb < 16 && c == 'x') || (c == 'X' && nb < 10))
+		ft_putchar(BASE16[nb]);
+	else if (nb > 9 && nb < 16 && c == 'X')
+		ft_putchar(BASE16[nb] - 32);
+	return (0);
+}
+
+int	ft_putnbr(int n)
+{
+	long int	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+	return (0);
+}
 
 int	ft_conversions(char c, va_list argp)
 {
-	if (c == 'c')
+	if (c == '%')
+		ft_putchar('%');
+	else if (c == 'c')
 		ft_putchar(va_arg(argp, int));
-	if (c == 's')
+	else if (c == 's')
 		ft_putstr(va_arg(argp, char *));
-	if (c == 'x')
-		ft_convertdectohexa(va_arg(argp, char *));
-	return (0);
+	else if (c == 'x' || c == 'X')
+		ft_convert10to16(va_arg(argp, int), c);
+	else if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(argp, int));
+	return (0);	
 }
 
 int	ft_printf(const char *format, ...)
@@ -52,47 +89,39 @@ int	ft_printf(const char *format, ...)
 	va_start(argp, format);
 	while (*format)
 	{
-		while (*format != '%' && *format)
-		{
-			ft_putchar(*format);
-			format++;
-		}
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '%')
-				ft_putchar('%');
-			else
-				ft_conversions(*format, argp);
-			format++;
+			ft_conversions(*format, argp);
 		}
+		else 
+			ft_putchar(*format);
+		format++;
 	}
 	va_end(argp);
 	return (0);
 }
 
-int	ft_convertdectohexa(int nb)
+int main(void)
 {
-	if (nb >= 16)
-	{
-		ft_convertdectohexa(nb / 16);
-		ft_convertdectohexa(nb % 16);
-	}
-	else
-		ft_putchar(BASE16[nb]);
-	return (nb);
+	ft_printf("|test pourcemtage=%%|\n");
+	printf("|test pourcemtage=%%|\n\n");
+
+	ft_printf("|test char a=%c|\n", 'a');
+	printf("|test char a=%c|\n\n", 'a');
+
+	ft_printf("|test str bonjour=%s|\n", "bonjour");
+	printf("|test str bonjour=%s|\n\n", "bonjour");
+
+	ft_printf("|test str vide=%s|\n", "");
+	printf("|test str vide=%s|\n\n", "");
+
+	ft_printf("|test hexa lower=%x|\n", 1516);
+	printf("|test hexa lower=%x|\n\n", 1516);
+
+	ft_printf("|test hexa lower=%X|\n", 1516);
+	printf("|test hexa lower=%X|\n\n", 1516);
+
+	ft_printf("|test decimal 1516=%d|\n", 1516);
+	printf("|test decimal 1516=%d|\n\n", 1516);
 }
-
-// int main(void)
-// {
-// 	// char *base ="0123456789abcdef";
-
-// 	// 11 / 16  
-
-// 	// printf("%c, %s, %p, ");
-
-// 	// ft_printf("bon%/  %c ecr%sire %c%c%c a\n", 'a', "bonj", 'b', 'c', 'd');
-// 	printf("bon%/  %c ecr%sire %c%c%c a\n", 'a', "bonj", 'b', 'c', 'd');
-// 	// printf("%d", ox);
-// 	ft_convertdectohexa(1516);
-// }
